1f72fb879ae5b2318ca9857c44d7665c9b972344
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2021-02-02 10:55:27 +0100

    problema closed #1

--- src/com/cod/Patricia.java
+++ src/com/cod/Patricia.java
@@ -3,5 +3,6 @@ package com.cod;
 public class Patricia {
     public void metodo(){
         System.out.println("Acabe la funcionalidad");
+        System.out.println("Problema cerrado ");
     }
 }
