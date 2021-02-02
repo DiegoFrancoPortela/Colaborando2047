a58892ff475141711e80048da24f1f71035bebef
mplacesalvarez <mplacesalvarez@danielcastelaor.org>
2021-02-02 11:27:47 +0100

    Problema 3 fix #3

--- src/com/cod/Manuel.java
+++ src/com/cod/Manuel.java
@@ -5,6 +5,5 @@ public class Manuel {
     public static void main(String[] args) {
         System.out.println("Primera funcionalidad terminada");
         System.out.println("Problema cerrado");
-
     }
 }
